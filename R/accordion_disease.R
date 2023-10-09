7498810d15237b027f3d0466248b784c47b2910b
Emma Busarello <emma.busarello@outlook.it>
2023-10-09 14:00:27 +0200

    Commit

--- R/accordion_disease.R
+++ R/accordion_disease.R
@@ -643,7 +643,7 @@ accordion_disease<-function(data,
         accordion_output<-append(accordion_output,info_list)
       }
     }
-    if ("cell" %in% annotation_resolution){
+    if ("cell" %in% annotation_resolution ){
 
       dt_top_ct_by_cell<-final_dt[order(-diff_score)][,head(.SD, n_top_celltypes),cell]
       dt_top_ct_per_cell<-as.data.table(dt_top_ct_by_cell)[,c("cell","cell_type","diff_score")]
@@ -657,30 +657,52 @@ accordion_disease<-function(data,
       cell_res_detailed_annotation_info[["cell_resolution"]][["detailed_annotation_info"]][["top_celltypes"]] <- as.data.table(dt_top_ct_per_cell)
 
       dt_top_marker<-unique(merge.data.table(dt_score,anno_dt_cell, by=c("cell_type","cell")))
-      dt_top <- unique(dt_top_marker[, quantile_score_marker := quantile(score,probs = top_marker_score_quantile_threshold, na.rm=TRUE), by=c("marker","marker_type","annotation_per_cell")][,c("cell_type","marker","marker_type","quantile_score_marker","cell","score","EC_score","specificity","annotation_per_cell", "diff_score")])
 
       if("cell" %in% group_markers_by){
+        dt_top <- unique(dt_top_marker[, quantile_score_marker := quantile(score,probs = top_marker_score_quantile_threshold, na.rm=TRUE), by=c("marker","marker_type","cell")][,c("cell_type","marker","marker_type","quantile_score_marker","cell","score","weight","specificity","annotation_per_cell", "diff_score")])
         #for each cell retrieves first N cell type and first N markers
         dt_top_marker_by_cell<-dt_top[order(-quantile_score_marker)][,head(.SD, n_top_markers),cell]
 
-        dt_top_marker_per_cell<-as.data.table(dt_top_marker_by_cell)[,c("cell","annotation_per_cell","diff_score","marker","marker_type","EC_score","specificity","score")]
-        colnames(dt_top_marker_per_cell)<-c("cell",eval(name),eval(name_score), "marker","marker_type","EC_score","specificity", "gene_impact_score_per_cell")
+        dt_top_marker_per_cell<-as.data.table(dt_top_marker_by_cell)[,c("cell","annotation_per_cell","diff_score","marker","marker_type","weight","specificity","score")]
+        colnames(dt_top_marker_per_cell)<-c("cell",eval(name),eval(name_score), "marker","marker_type","weight","specificity", "gene_impact_score_per_cell")
 
         cell_res_detailed_annotation_info[["cell_resolution"]][["detailed_annotation_info"]][["top_markers_per_cell"]] <- as.data.table(dt_top_marker_per_cell)
 
       }
       if("celltype_cell" %in% group_markers_by){
         #for each cell retrieves first N cell type and first N markers
-        dt_top_marker_by_cell<-dt_top[order(-quantile_score_marker)][,head(.SD, n_top_markers),annotation_per_cell]
+        if(!is.null(condition_info)){
+          if(data_type == "seurat"){
+            condition_table<-data@meta.data
+            condition_table<-as.data.table(condition_table)[,cell:=rownames(condition_table)]
+            condition_table<-condition_table[,c("cell","condition")]
 
-        name<-paste0(annotation_name,"_per_cell")
-        name_score<-paste0(annotation_name,"_per_cell_score")
+          } else{
+            condition_table<-as.data.table(condition_info)[,c("cell","condition")]
 
-        dt_top_marker_per_cell<-as.data.table(dt_top_marker_by_cell)[,c("cell","annotation_per_cell","diff_score","marker","marker_type","EC_score","specificity","score","quantile_score_marker")]
-        colnames(dt_top_marker_per_cell)<-c("cell",eval(name),eval(name_score), "marker","marker_type","EC_score","specificity", "gene_impact_score_per_cell","gene_impact_score_per_celltype_cell")
+          }
+          dt_top_marker_condition<-merge(dt_top_marker, condition_table, by="cell")
+          dt_top <- unique(dt_top_marker_condition[, quantile_score_marker := quantile(score,probs = top_marker_score_quantile_threshold, na.rm=TRUE), by=c("marker","marker_type","annotation_per_cell","condition")][,c("condition","annotation_per_cell","marker","marker_type","quantile_score_marker","weight","specificity")])
+          dt_top<-unique(dt_top[,c("annotation_per_cell","condition","marker","marker_type","quantile_score_marker","weight","specificity")])
+          dt_top_marker_by_cell<-dt_top[order(-quantile_score_marker)][,head(.SD, n_top_markers),c("annotation_per_cell","condition")]
 
-        cell_res_detailed_annotation_info[["cell_resolution"]][["detailed_annotation_info"]][["top_markers_per_celltype_cell"]] <- as.data.table(dt_top_marker_per_cell)
+          name<-paste0(annotation_name,"_per_cell")
+          name_score<-paste0(annotation_name,"_per_cell_score")
 
+          colnames(dt_top_marker_by_cell)<-c(eval(name), eval(condition_info),"marker","marker_type","gene_impact_score_per_celltype_cell","weight","specificity")
+
+          cell_res_detailed_annotation_info[["cell_resolution"]][["detailed_annotation_info"]][["top_markers_per_celltype_cell"]] <- as.data.table(dt_top_marker_by_cell)
+        } else {
+          dt_top <- unique(dt_top_marker[, quantile_score_marker := quantile(score,probs = top_marker_score_quantile_threshold, na.rm=TRUE), by=c("marker","marker_type","annotation_per_cell")][,c("cell","annotation_per_cell","marker","marker_type","quantile_score_marker","weight","specificity")])
+          dt_top_marker_by_cell<-unique(dt_top[,c("annotation_per_cell","marker","marker_type","quantile_score_marker","weight","specificity")])
+          dt_top_marker_by_cell<-dt_top_marker_by_cell[order(-quantile_score_marker)][,head(.SD, n_top_markers),annotation_per_cell]
+          name<-paste0(annotation_name,"_per_cell")
+          name_score<-paste0(annotation_name,"_per_cell_score")
+
+          colnames(dt_top_marker_by_cell)<-c(eval(name), "marker","marker_type","gene_impact_score_per_celltype_cell","weight","specificity")
+
+          cell_res_detailed_annotation_info[["cell_resolution"]][["detailed_annotation_info"]][["top_markers_per_celltype_cell"]] <- as.data.table(dt_top_marker_by_cell)
+        }
       }
       if(data_type == "seurat"){
         if(is_empty(data@misc[[annotation_name]])){
