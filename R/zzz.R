e311850b3730f3ca2767de75437fa4340a6b5977
emma busarello <emma.busarello@outlook.it>
2024-12-06 16:18:26 +0100

    Commit

--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-owlready2 <- NULL
-.onLoad <- function(libname, pkgname) {
-  # use superassignment to update global reference to scipy
-  owlready2 <<- reticulate::import("owlready2", delay_load = TRUE)
-}
