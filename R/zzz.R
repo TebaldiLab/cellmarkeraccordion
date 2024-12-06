3dd9e59e4b485be0feb206cbd35aa6ca1463f860
emma busarello <emma.busarello@outlook.it>
2024-12-06 15:50:39 +0100

    Commit

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,15 @@
 .onLoad <- function(libname, pkgname) {
   library(reticulate)
 
-  # Check if Python is available; if not, configure Miniconda automatically
+  # Suppress reticulate prompt
+  options(reticulate.miniconda.prompt = FALSE)
+
+  # Check or configure Python environment
   if (!py_available(initialize = FALSE)) {
-    # Automatically install Miniconda if needed
     if (!miniconda_exists()) {
       install_miniconda()
     }
     use_miniconda(required = TRUE)
   }
+
 }
