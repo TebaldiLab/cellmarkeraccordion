d841090e7b681de00c3dc30fe1b448f976e86062
emma busarello <emma.busarello@outlook.it>
2024-12-11 15:18:55 +0000

    Commit

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -6,15 +6,6 @@
 #' @format A data table
 NULL
 
-#' bone_marrow_data: Example dataset of human bone marrow from
-#'
-#' @docType data
-#' @name bone_marrow_data
-#' @usage data(bone_marrow_data)
-#' @format A Seurat object
-NULL
-
-
 #' cell_onto: Cell ontology
 #'
 #' @docType data
@@ -31,62 +22,12 @@ NULL
 #' @format A data table
 NULL
 
-#'
-#' @docType data
-#' @name cell_onto
-#' @usage data(cell_onto)
-#' @format Cell ontology
-NULL
-
-
-#' counts: PBMC raw counts
-#'
-#' @docType data
-#' @name counts
-#' @usage data(counts)
-#' @format matrix
-NULL
-
 #' disease_accordion_marker: Cell Marker Accordion disease database
 #'
 #' @docType data
 #' @name disease_accordion_marker
 #' @usage data(disease_accordion_marker)
 #' @format A data table
-NULL
-
-
-#' marker_table: Table of markers associated to retinal cell types
-#'
-#' @docType data
-#' @name marker_table
-#' @usage data(marker_table)
-#' @format A data table
-NULL
-
-#' marker_table_pathway: Table of markers associated to apoptosis
-#'
-#' @docType data
-#' @name marker_table_pathway
-#' @usage data(marker_table_pathway)
-#' @format A data table
-NULL
-
-#' mouse_data: Mouse single-cell dataset
-#'
-#' @docType data
-#' @name mouse_data
-#' @usage data(mouse_data)
-#' @format A Seurat object
-NULL
-
-
-#' retinal_data: Retina single-cell dataset
-#'
-#' @docType data
-#' @name retinal_data
-#' @usage data(retinal_data)
-#' @format A Seurat object
 NULL
 
 #' uberon_onto: Uberon anatomy ontology
