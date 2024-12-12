3e488dc74491e9a875e4b2fe8220a43f59f7146f
emma busarello <emma.busarello@outlook.it>
2024-12-12 13:26:54 +0100

    Commit

--- R/accordion_plot.R
+++ R/accordion_plot.R
@@ -576,7 +576,6 @@ accordion_plot<-function(data,
                                   arrow = arrow(type = "closed", length = unit(3, 'mm')))+
                    geom_label(aes(x = x, y = y, label = name), nudge_y = 0.1, label.size = NA,size = bs*0.28, colour=V(onto_igraph)$color, label.padding = unit(0.1, "lines")) +
                    theme_graph()+
-                   ylim(-3, 3) +
                    xlim(-3, 3)
                 como_plot<-plot_grid(pl, tree_plot, rel_widths = c(1, 2), nrow=1, scale = c(1, 1))
 
