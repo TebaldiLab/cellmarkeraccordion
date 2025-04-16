d638c620b2a9e05b44abc74a8e4ef851f1bd4c67
emma busarello <emma.busarello@outlook.it>
2025-04-16 16:42:09 +0200

    Updates

--- R/accordion_custom.R
+++ R/accordion_custom.R
@@ -392,6 +392,8 @@ accordion_custom<-function(data,
   marker_table<-marker_table[marker_type=="positive",SPs_reg := scales::rescale(as.numeric(SPs), to = c(1,length_ct_pos),from = c(length_ct_pos,1))
   ][marker_type=="negative",SPs_reg := scales::rescale(as.numeric(SPs), to = c(1,length_ct_neg),from = c(length_ct_neg,1))
   ][,c("cell_type","marker","marker_type","SPs","SPs_reg","weight_scaled","weight")]
+
+  marker_table[,SPs:=as.numeric(format(round(1/SPs,2), nsmall=2))]
   marker_table[,SPs_reg:=log10(SPs_reg)+1]
 
   setkey(marker_table,marker,cell_type)
